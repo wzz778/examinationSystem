188c781dcdbbdb458a8cd68c55db7ed93035ad0d
XY <3400164864@qq.com>
2022-11-30 22:45:18 +0800

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import a from "./myAxios/index";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    console.log(a);
+    a.getUserInfo()
+      .then((result) => {
+        console.log("结果", result);
+      })
+      .catch((err) => {
+        console.log("失败", err);
+      });
+  },
+};
 </script>
 
 <style>
